38a3f482d6eec75663bc51e9681abbcf54da4327
Jieun Jeon <jieun5230@naver.com>
2022-03-23 17:54:29 +0900

    update userProfile ...ing

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,38 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
 	Mutation: {
-		editProfile: () => console.log("editprofile success"),
+		editProfile: async (
+			_,
+			{ firstName, lastName, username, email, password: newPassword }
+		) => {
+			let uglyPassword = null;
+			if (newPassword) {
+				uglyPassword = await bcrypt.hash(newPassword, 10);
+			}
+
+			const updatedUser = await client.user.update({
+				where: { id: 1 },
+				data: {
+					firstName,
+					lastName,
+					username,
+					email,
+					...(uglyPassword && { password: uglyPassword }), // hashPassword true -> password: hashPassword(es6문법)
+				},
+			});
+
+			if (updatedUser) {
+				return {
+					ok: true,
+				};
+			} else {
+				return {
+					ok: false,
+					error: "Could not update your profile",
+				};
+			}
+		},
 	},
 };
