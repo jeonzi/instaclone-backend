13aa838d4840aec7bff4fc58e8949cece0deafb3
Jieun Jeon <jieun5230@naver.com>
2022-04-09 23:32:40 +0900

    Following User

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,37 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+	Mutation: {
+		followUser: protectedResolver(
+			async (_, { username }, { loggedInUser }) => {
+				const exsitngUser = await client.user.findUnique({
+					where: { username },
+				});
+
+				if (!exsitngUser) {
+					return {
+						ok: false,
+						error: "That user does not exsit. Can't follow",
+					};
+				}
+				await client.user.update({
+					where: {
+						id: loggedInUser.id,
+					},
+					data: {
+						following: {
+							connect: {
+								//unique 한 속성이어야 함
+								username,
+							},
+						},
+					},
+				});
+				return {
+					ok: true,
+				};
+			}
+		),
+	},
+};
