7c3ac9754ab37f09bfe25c16ead84bd2576af80c
Jieun Jeon <jieun5230@naver.com>
2022-04-10 16:32:37 +0900

    seeFollowers with Pagination(offset pagination)

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,19 +3,37 @@ import client from "../../client";
 export default {
 	Query: {
 		seeFollowers: async (_, { username, page }) => {
-			const aFollowers = await client.user
+			const exsitingUser = await client.user.findUnique({
+				where: { username },
+				select: { id }, // select: 원하는 특정 필드만 가져오는 기능(최소 하나는 넣어줘야 함)
+			});
+			if (!exsitingUser) {
+				return {
+					ok: false,
+					error: "User not found",
+				};
+			}
+			const followers = await client.user
 				.findUnique({
 					where: {
 						username,
 					},
 				})
+				/**
+				 *  offset pagination
+				 *  Offset pagination은 건너뛰기 및 가져오기를 사용하여 특정 수의 결과를 건너뛰고 제한된 범위를 선택합니다.
+				 */
 				.followers({
 					take: 5,
 					skip: (page - 1) * 5,
 				});
+			const totalFollwers = await client.user.count({
+				where: { following: { some: { username } } },
+			});
 			return {
 				ok: true,
 				followers,
+				totalPages: Math.ceil(totalFollwers / 5),
 			};
 		},
 	},
