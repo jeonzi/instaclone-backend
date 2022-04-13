f906153549892604b64fffc5a159a5a05abf2f47
Jieun Jeon <jieun5230@naver.com>
2022-04-14 02:29:11 +0900

    SeeHashtag and photos computeField

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -1,3 +1,4 @@
+import { argsToArgsConfig } from "graphql/type/definition";
 import client from "../client";
 
 export default {
@@ -10,6 +11,30 @@ export default {
 			client.hashtag.findMany({
 				where: {
 					photos: {
+						some: {
+							id,
+						},
+					},
+				},
+			}),
+	},
+	Hashtag: {
+		photos: ({ id }, { page }) => {
+			return client.hashtag
+				.findUnique({
+					where: {
+						id,
+					},
+				})
+				.photos();
+		},
+		totalPhotos: (
+			{ id },
+			{ loggedInUser } // 부분적으로 protect할 수도 있다
+		) =>
+			client.photo.count({
+				where: {
+					hashtags: {
 						some: {
 							id,
 						},
